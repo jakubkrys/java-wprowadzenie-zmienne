16b5a8d232149b4b7ba1e42587d12de280173dc2
Jakub <jakubkrys@gmail.com>
2020-12-05 22:54:14 +0100

    Prezentacja roku urodzenia i płci

---
+++ App.java
@@ -0,0 +1,10 @@
+class App {
+	public static void main(String args[]){
+		int birthYear = 2001;
+		char gender = 'K';
+		
+		System.out.println("Birth year: "+birthYear);
+		System.out.println();
+		System.out.println("Gender: "+gender);
+	}
+}
