d3e6b712a605fa8849cc872873865ef8109a735e
Jakub <jakubkrys@gmail.com>
2020-12-05 23:09:40 +0100

    Dodanie zadania 2

--- App.java
+++ App.java
@@ -1,10 +1,21 @@
 class App {
 	public static void main(String args[]){
+		// Zadanie 1
 		int birthYear = 2001;
 		char gender = 'K';
-		
+		System.out.println("-----Zadanie 1-----");
 		System.out.println("Birth year: "+birthYear);
-		System.out.println();
 		System.out.println("Gender: "+gender);
+		System.out.println("-------------------------");
+		System.out.println();
+		
+		// Zadanie 2
+		boolean isAdult = true;
+		double averageGrade = 3.5;
+		System.out.println("-----Zadanie 2-----");
+		System.out.println("Is adult?: "+isAdult);
+		System.out.println("Average grade: "+averageGrade);
+		System.out.println("-------------------------");
+		System.out.println();
 	}
 }
